8ae5f7f9d4e6a2635b8e596cbb403a7fe61fc7f7
Siddhanth B N <bnsiddhanth0@gmail.com>
2024-04-13 17:08:08 +0530

    #1: echo

---
+++ echo/go.mod
@@ -0,0 +1,5 @@
+module echo
+
+go 1.22.1
+
+require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240408130303-0186f398f965 // indirect
---
+++ echo/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	node := maelstrom.NewNode()
+
+	node.Handle("echo", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "echo_ok"
+		body["in_reply_to"] = body["msg_id"]
+
+		return node.Reply(msg, body)
+	})
+
+	if err := node.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
