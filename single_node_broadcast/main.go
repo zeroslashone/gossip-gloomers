d7fc30388edaf8370efc6682b5e97054f4e6355d
Siddhanth B N <bnsiddhanth0@gmail.com>
2024-04-14 16:08:25 +0530

    #3b: broadcast

--- single_node_broadcast/main.go
+++ single_node_broadcast/main.go
@@ -7,9 +7,16 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type TopologyBody struct {
+	MsgId    int                 `json:"msg_id"`
+	Type     string              `json:"type"`
+	Topology map[string][]string `json:"topology"`
+}
+
 func main() {
 	messages := []int{}
 	node := maelstrom.NewNode()
+	neighbors := []string{}
 
 	node.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -18,7 +25,21 @@ func main() {
 			return err
 		}
 
-		messages = append(messages, int(body["message"].(float64)))
+		message := int(body["message"].(float64))
+		present := false
+		for _, m := range messages {
+			if m == message {
+				present = true
+				break
+			}
+		}
+		if !present {
+			messages = append(messages, message)
+
+			for _, nid := range neighbors {
+				node.Send(nid, msg.Body)
+			}
+		}
 
 		body["type"] = "broadcast_ok"
 		body["in_reply_to"] = body["msg_id"]
@@ -41,16 +62,20 @@ func main() {
 	})
 
 	node.Handle("topology", func(msg maelstrom.Message) error {
-		var body map[string]any
+		var body TopologyBody
+		respBody := map[string]any{}
 
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		body["type"] = "topology_ok"
-		body["in_reply_to"] = body["msg_id"]
-		delete(body, "topology")
+		neighbors = body.Topology[node.ID()]
+		log.Println("Neighbors are: ", neighbors)
 
-		return node.Reply(msg, body)
+		respBody["type"] = "topology_ok"
+		respBody["in_reply_to"] = body.MsgId
+		respBody["msg_id"] = body.MsgId
+
+		return node.Reply(msg, respBody)
 	})
 
 	if err := node.Run(); err != nil {
